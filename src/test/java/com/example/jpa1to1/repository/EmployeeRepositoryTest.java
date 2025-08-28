1a9349a7df404de9673dc75f82e8986fe4f3de78
Malthe-kea <mati0002@stud.kea.dk>
2025-08-28 11:21:59 +0200

    testDeleteUser() passer

--- src/test/java/com/example/jpa1to1/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/jpa1to1/repository/EmployeeRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.example.jpa1to1.repository;
 
 import com.example.jpa1to1.model.Employee;
+import org.hibernate.JDBCException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 import java.util.List;
 
@@ -13,10 +15,25 @@ class EmployeeRepositoryTest {
 
     @Autowired
     EmployeeRepository employeeRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void testAtLeastOneJens() {
         List<Employee>  lst = employeeRepository.findEmployeeByName("Jens");
         assertTrue(lst.size()>0);
+    }
+
+    @Test
+    void testDeleteUser() {
+        //Denne test forudsætter at InitData har kørt
+        //Vi har en user og en employee i databasen
+        //Vi vil her teste at vi IKKE kan slette en user der har en employee
+        List<Employee>  lst = employeeRepository.findEmployeeByName("Jens");
+        Employee emp1 = lst.get(0);
+        assertEquals("Jens", emp1.getName());
+        //userRepository.delete(emp1.getUser());
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> userRepository.delete(emp1.getUser()));
+
     }
 }
