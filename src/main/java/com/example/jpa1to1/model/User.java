f8e6ca6d7a56e883342a034bc6429048f5b982d2
Malthe-kea <mati0002@stud.kea.dk>
2025-08-28 10:00:05 +0200

    ininit

---
+++ src/main/java/com/example/jpa1to1/model/User.java
@@ -0,0 +1,4 @@
+package com.example.jpa1to1.model;
+
+public class User {
+}
---
+++ src/test/java/com/example/jpa1to1/Jpa1to1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa1to1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa1to1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
